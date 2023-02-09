2c7dcd2d6b0165033fea3d6fdd35fc08cb553cc7
maxsainsbury <maxwell.sainsbury@gmail.com>
2023-02-09 08:21:27 -0800

    got data to display properly

--- src/components/ContentBox/ContentBox.js
+++ src/components/ContentBox/ContentBox.js
@@ -7,23 +7,47 @@ export function ContentBox({index}) {
   let data = useSelector((state) => state.content.content[index].data);
   console.log(data);
 
-  
+  const whatsInBody = (data) => {
+    if (data.is_reddit_media_domain) {
+      if (data.is_video) {
+        return (
+          <video className="media" controls>
+            <source src={data.media.reddit_video.fallback_url} />  
+          </video> 
+        )
+      }
+      else {
+        return (
+          <img className="media" src={data.url} />
+        )
+      }
+    }
+    else if (data.url.includes('imgur')) {
+      return (
+        <img className="media" src={data.url} />
+      )
+    }
+    else if (!data.url.includes('reddit')) {
+      return (
+        <a className="outsideLink" href={data.url}>{data.url}</a>
+      )
+    }
+  }
 
   return (
     <div className="content-box">
-      <h2>{data.title}</h2>
-      {data.is_video === false ? <img src={data.url} /> : 
-      <video controls>
-      <source src={data.media.reddit_video.fallback_url} />  
-      </video>}
+      <h2>{(data.over_18 === true) 
+      ? <span className="NSFW">NSFW</span> 
+      : <span></span>} {data.title}</h2>
+
+      {whatsInBody(data)}
+
+      <p className="selftext">{data.selftext}</p>
       <div className="info">
         <p className="item" title="karma">{data.score} Karma</p>
-        <div className="item" title="user">
-          <p>Posted on {data.subreddit_name_prefixed}</p>
-          <p>By {data.author}</p>
-        </div>
-        <p className="item" title="time">{} hours ago</p>
-        <a href="#" className="item" title="comments">{data.num_comments} Comments</a>
+        <p className="item">Posted on {data.subreddit_name_prefixed}</p>
+        <p className="item">By {data.author}</p>
+        <a href={`https://www.reddit.com${data.permalink}`} className="item" title="comments">{data.num_comments} Comments</a>
       </div>
     </div>
 
